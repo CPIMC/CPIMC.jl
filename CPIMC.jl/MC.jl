00ca5faec369a792c1b55d6d7dceb0dbfb58f062
K. Hunger <hunger@physik.uni-kiel.de>
2020-09-03 18:27:09 +0200

    Modify MC.jl

--- CPIMC.jl/MC.jl
+++ CPIMC.jl/MC.jl
@@ -1,41 +1,30 @@
-function sweep(steps::Int, sampleEvery::Int, updates, measurements, e::Ensemble, c::Configuration)
-  nUpdates = length(updates)
+function sweep(steps::Int, sampleEvery::Int, measurements, e::Ensemble, c::Configuration)
+    # get_update()
 
-  @assert nUpdates > 0
+    i = 0
 
-  updatesProposed = zeros(Int, nUpdates)
-  updatesAccepted = zeros(Int, nUpdates)
+    while i < steps
 
-  i = 0
-
-  while i < steps
-    k = rand(1:nUpdates)
-
-    result = updates[k](e,c)
-
-    updatesProposed[k] += 1
-    
-    if result == :drop
-      continue
-    end
-
-    if result == :accept
-      updatesAccepted[k] += 1
-    end
-
-    if i % sampleEvery == 0
-      for (stat,obs) in measurements
-        if typeof(stat) == Group
-          fit!(stat, eachrow(obs(e,c)))
+        if rand() > get_update(c)
+            ## accept
+            accept(c)
         else
-          fit!(stat, obs(e,c))
+            ## deny
+            deny(c)
         end
-      end
-    end
+
+        if i % sampleEvery == 0
+            for (stat,obs) in measurements
+                if typeof(stat) == Group
+                    fit!(stat, eachrow(obs(e,c)))
+                else
+                    fit!(stat, obs(e,c))
+                end
+            end
+        end
 
     i += 1
   end
 
   return zip(updatesAccepted,updatesProposed)
 end
-
--- CPIMC.jl/models/grand_canonical/model.jl
+++ CPIMC.jl/models/grand_canonical/model.jl
@@ -20,22 +20,22 @@ mutable struct Configuration
   N :: Int
 end
 
-# function K(n::Int, e::Ensemble, c::Configuration)
-#   return pi^2 / (2 * e.L^2) * n^2
-# end
-#
-# function emptyOrbs(e::Ensemble, c::Configuration)
-#   return filter(x -> !(x in c.occupations), 1:e.cutoff)
-# end
-#
-# function occVec(e::Ensemble, c::Configuration)
-#   return map(x -> Int(x in c.occupations), 1:e.cutoff)
-# end
-#
-# function totalEnergy(e::Ensemble, c::Configuration)
-#   return sum(map(x -> K(x,e,c), collect(c.occupations)))
-# end
-#
-# function particleNumber(e::Ensemble, c::Configuration)
-#   return c.N
-# end
+function K(n::Int, e::Ensemble, c::Configuration)
+  return pi^2 / (2 * e.L^2) * n^2
+end
+
+function emptyOrbs(e::Ensemble, c::Configuration)
+  return filter(x -> !(x in c.occupations), 1:e.cutoff)
+end
+
+function occVec(e::Ensemble, c::Configuration)
+  return map(x -> Int(x in c.occupations), 1:e.cutoff)
+end
+
+function totalEnergy(e::Ensemble, c::Configuration)
+  return sum(map(x -> K(x,e,c), collect(c.occupations)))
+end
+
+function particleNumber(e::Ensemble, c::Configuration)
+  return c.N
+end
