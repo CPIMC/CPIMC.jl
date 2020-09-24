03cbbaddda033030ac3915878859a76aa1713799
Rif <be41@hotmail.de>
2020-09-24 12:26:17 +0200

    Added differnt functions in Configuration to acces relevant quanteties for the interacting System, while using the minimal datastructure for an interacting configuration.

--- CPIMC.jl/src/Configuration.jl
+++ CPIMC.jl/src/Configuration.jl
@@ -2,19 +2,19 @@ using DataStructures
 
 
 struct T2{T}
-  " start orbital "
+  " creator "
   i :: T
 
-  " end orbital "
+  " annihilator "
   j :: T
 end
 
 struct T4{T}
-  " start orbitals "
+  " creator "
   i :: T
   j :: T
 
-  " end orbitals "
+  " annihilator "
   k :: T
   l :: T
 end
@@ -33,4 +33,53 @@ mutable struct Configuration{T}
   Configuration(s::Set{T}) where {T} = new{T}(s,SortedDict{Float64,Kink}(Base.Forward))
 end
 
+function change_occupations(occs::Set, K::T4)
+  assert(in(K.k, occs) & in(K.l, occs))
+  assert(!in(K.i, occs) & !in(K.j, occs))
+  delete!(occs, k)
+  delete!(occs, l)
+  push!(occs, i)
+  push!(occs, j)
+end
 
+function get_occupations_at(conf::Configuration, Tau::Float64)
+  occupations = copy(conf.occupations)
+  for (tau_kink,kink) in conf.kinks
+    if tau_kink < Tau
+      change_occupations(occupations, kink)
+    else
+      break
+    end
+  end
+  return occupations
+end
+
+function get_kinks_of(Configuration::Configuration, orbital::Orbital)
+  kinks = SortedDict{}
+  for (tau_kink,kink) in Configuration.kinks
+    if kink.i == orbital | kink.j == orbital | kink.k == orbital | kink.l == orbital
+      kinks{Tau_Kink} = kink
+    end
+  end
+  return kinks
+end
+
+function is_non_interacting(Configuration::Configuration, orbital::Orbital)
+  for tau_kink,kink in Configuration.kinks
+    if kink.i == orbital | kink.j == orbital | kink.k == orbital | kink.l == orbital
+      return(false)
+    end
+  end
+  return(true)
+end
+
+
+function get_non_interacting_orbs_of_set(Configuration, os::Set{Orbital})
+  non_int_orbs = Set{Orbital}()
+  for orb in os:
+    if is_non_interacting(Configuration, orb)
+      push!(non_int_orbs, orb)
+    end
+  end
+  return(non_int_orbs)
+end
