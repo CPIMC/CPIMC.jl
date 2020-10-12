3fe293b3ad8e1e1d709ded3109db62477f2e8e22
K. Hunger <hunger@physik.uni-kiel.de>
2020-10-12 10:42:37 +0200

    Add plotscript for OccNumns.

--- CPIMC.jl/test/HEG/plot_occNums.jl
+++ CPIMC.jl/test/HEG/plot_occNums.jl
@@ -1,7 +1,8 @@
 using DelimitedFiles
 using Plots
 
-data = readdlm("out/occNums_N33_th10_rs05.dat")
+data = readdlm("out/occNums_N33_th10_rs05.dat")# load occupation number results
+degn = readdlm("data/degeneracy_3d.dat")# load degeneracy
 
 ti = .!iszero.(data[:,2])# select non-zero results to match with degeneracy_3c
 x = sqrt.(data[ti,1])#square-root: single-particle energy → absolute of momentum
@@ -9,6 +10,8 @@ y = data[ti,2]
 y = y./degn[1:length(y)]# divide by degeneracy
 y = y./sum(y)# normalize bins
 
-degn = readdlm("data/degeneracy_3d.dat")
+plot(x, y, xlabel="k", ylabel="n(k)", lw=4, label="n_k")
+vline!([2], label="kF", lw=4, la=0.7)## N=33 ↔ EF=4 → kF=2
+title!("Θ=1, rs=0.5, N=33")
 
-plot(x, y, legend=false, xlabel="k", ylabel="n(k)")
+savefig("out/plots/occNums_N33_th10_rs05.pdf")
