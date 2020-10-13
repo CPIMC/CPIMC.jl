abd078b6447496eb8336474351c1dccc439887b0
Kai Hunger <hunger@physik.uni-kiel.de>
2020-10-13 18:04:21 +0200

    Add occupation estimator.

--- CPIMC.jl/src/HEG/Ideal/estimators.jl
+++ CPIMC.jl/src/HEG/Ideal/estimators.jl
@@ -6,6 +6,16 @@ function occVec(c::Configuration)
     get_energy.(c.occupations)
 end
 
+function occupations(c::Configuration, emax::Int=99)
+    nk = zeros(emax)
+    for en in 0:emax
+        if in(en, get_energy.(c.occupations))
+            nk[en+1] = 1
+        end
+    end
+    nk
+end
+
 function particleNumber(c::Configuration)
   return length(c.occupations)
 end
