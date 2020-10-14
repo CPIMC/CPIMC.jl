b0d09f9a141b558d9cf372b10d53afbfa0bb2f5d
K. Hunger <hunger@physik.uni-kiel.de>
2020-10-14 16:03:02 +0200

    Fix units in plot_occNums.jl
    Keep entire plot in internal (integer) units.

--- CPIMC.jl/test/HEG/plot_occNums.jl
+++ CPIMC.jl/test/HEG/plot_occNums.jl
@@ -4,7 +4,7 @@ using Plots
 data = readdlm("out/occNums_N33_th10_rs05.dat")# load occupation number results
 degn = readdlm("data/degeneracy_3d.dat")# load degeneracy
 
-x = 0.5 .* sqrt.(0:length(data[:,1])-1)# square-root: single-particle energy → absolute of momentum
+x = sqrt.(0:length(data[:,1])-1)# square-root: single-particle energy → absolute of momentum
 y = data[:,1]
 dy = data[:,2]
 ti = .!iszero.(y)# select non-zero results to match with degeneracy_3d.dat
@@ -13,9 +13,9 @@ y = y[ti]
 y = y./degn[1:length(y)]# divide by degeneracy
 dy = dy[ti]
 dy = dy./degn[1:length(y)]# divide by degeneracy
-sy = sum(y)
-y = y./sy# normalize bins
-dy = dy./sy
+
+println("Normalization : sum(n_k) = $(sum(y.*degn[1:length(y)]))")
+
 plot(x, y, ribbon=dy, xlabel="k", ylabel="n(k)", lw=4, label="n_k", ylims=(0,1))
 vline!([2], label="kF", lw=4, la=0.7)## N=33 ↔ EF=4 → kF=2
 title!("Θ=1, rs=0.5, N=33")
