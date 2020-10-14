1c648bea34387730e00111edf3e3db78fbd54e7f
K. Hunger <hunger@physik.uni-kiel.de>
2020-10-14 15:57:17 +0200

    Correct occNums estimator.
    Return the sum of ALL occurrences of each energy.

--- CPIMC.jl/src/HEG/Ideal/estimators.jl
+++ CPIMC.jl/src/HEG/Ideal/estimators.jl
@@ -6,11 +6,11 @@ function occVec(c::Configuration)
     get_energy.(c.occupations)
 end
 
-function occupations(c::Configuration, emax::Int=99)
+function occupations(c::Configuration, emax::Int=100)
     nk = zeros(emax)
 
     for en in get_energy.(c.occupations)
-        nk[en+1] = 1
+        nk[en+1] = nk[en+1] + 1
     end
 
     nk
