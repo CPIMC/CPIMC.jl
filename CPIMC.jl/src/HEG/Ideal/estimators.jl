6b9aa4cc3244b7afca5f12e26ed41c500ed3d672
K. Hunger <hunger@physik.uni-kiel.de>
2020-10-15 14:34:08 +0200

    Correct estimator for occupations.
    Filter energies of occupied orbitals for values smaller or equal than emax::Int=100, avoiding Array overflow.

--- CPIMC.jl/src/HEG/Ideal/estimators.jl
+++ CPIMC.jl/src/HEG/Ideal/estimators.jl
@@ -5,8 +5,11 @@ end
 function occupations(c::Configuration, emax::Int=100)
     nk = zeros(emax)
 
-    for en in get_energy.(c.occupations)
-        nk[en+1] = nk[en+1] + 1
+    ens = get_energy.(c.occupations)
+    ens = ens[ens .< emax]
+
+    for ε in ens
+        nk[ε+1] = nk[ε+1] + 1
     end
     nk
 end
