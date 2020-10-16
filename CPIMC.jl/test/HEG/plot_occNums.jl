2d73ca034d765a81aac715274f9979b68fca51d4
K. Hunger <hunger@physik.uni-kiel.de>
2020-10-16 18:58:55 +0200

    Adapt plot_occNums.jl to new results-file name.
    Also improve parsing of parameters from results-file name.

--- CPIMC.jl/test/HEG/plot_occNums.jl
+++ CPIMC.jl/test/HEG/plot_occNums.jl
@@ -34,33 +34,23 @@ function plot_occNums!(p::Plots.Plot, data, degn, N, Θ, rs; kwargs...)
     println("Normalization : sum(n_k) = $(sum(y.*degn[1:length(y)]))")
     println("for $(N) particles.")
 
-    plot(x, y, ribbon=dy, xlabel="k [internal units]", ylabel="n(k)", lw=4, label="n_k", ylims=(0,1))
+    plot(x, y, ribbon=dy, xlabel="k [internal units]", ylabel="n(k)", lw=4, label="n_k")
     vline!([kF(N)], label="kF=$(kF(N))", lw=4, la=0.7)
     title!("Θ=$(Θ), rs=$(rs), N=$(N)")
 end
 plot_occNums(args...; kwargs...) = plot_occNums!(plot(), args...; kwargs...)
 
-data = readdlm("out/occNums_N33_th20_rs05.dat")# load occupation number results
 degn = readdlm("data/degeneracy_3d.dat")# load degeneracy
 
 for file in readdir("./out/")
-    if occursin("occNums_N", file) & occursin(".dat", file)
-        n = parse(Int, split(split(file, "occNums_N")[2], "_th")[1])
+    if occursin("occs_N", file) & occursin(".dat", file)
+        n = parse(Int, split(split(file, "occs_N")[2], "_th")[1])
         θ = parse(Float64, split(split(file, "_th")[2], "_rs")[1])
-        if θ < 1
-            d = floor(log10(θ))
-            θ = θ * 10^(-d)
-        else
-            θ *= 0.1
-        end
+        θ /= 10^floor(log10(θ))
         r = parse(Float64, split(split(file, "_rs")[2], ".dat")[1])
-        if r < 1
-            d = floor(log10(r))
-            r = r * 10^(-d)
-        else
-            r *= 0.1
-        end
+        r /= 10^floor(log10(r))
+        data = readdlm(joinpath("out/", file))# load occupation number results
         plot_occNums(data, degn, n, θ, r)
-        savefig("out/plots/occNums_N$(n)_th$(replace(string(θ), "." => ""))_rs$(replace(string(r), "." => "")).pdf")
+        savefig("out/plots/occs_N$(n)_th$(replace(string(θ), "." => ""))_rs$(replace(string(r), "." => "")).pdf")
     end
 end
