59d5d214627d91f75f9d66eb7893e86719794bca
Kai Hunger <hunger@physik.uni-kiel.de>
2020-12-25 22:29:06 +0100

    fix test parameters.

--- CPIMC.jl/test/HEG/run.jl
+++ CPIMC.jl/test/HEG/run.jl
@@ -1,8 +1,8 @@
 using OnlineStats
 using DelimitedFiles
 
-include("../../src/HEG/model.jl")
 include("../../src/Configuration.jl")
+include("../../src/HEG/model.jl")
 include("../../src/HEG/Ideal/estimators.jl")
 include("../../src/CPIMC.jl")
 include("../../src/HEG/Ideal/updates.jl")
@@ -13,9 +13,12 @@ function simulation(NMC, cyc, NEquil, theta, rs, dε)
 
     S = get_sphere(Orbital((0,0,0),0),dε=dε)
     N = length(S)
+    println("===============================================================\n")
     println("Number of particles: ", N)
 
     c = Configuration(S)
+
+    println("initial configuration: $(c.occupations)")
 
     e = Ensemble(rs, get_beta_internal(theta,N), N) # get_beta_internal only works for 3D
 
@@ -26,24 +29,24 @@ function simulation(NMC, cyc, NEquil, theta, rs, dε)
     ,   :occs => (Group([Variance(UInt) for i = 1:100]), occupations)
     )
 
-    print("Start MC process ... ")
+    print("Start MC process ... \n")
     sweep(NMC, cyc, NEquil, updates, measurements, e, c)
     println(" finished.")
 
     print_results(measurements)
 
-    save_results("/home/vorlautv/git/cpimc2020/CPIMC.jl/test/HEG/out", measurements, e)
+    # save_results("/home/vorlautv/git/cpimc2020/CPIMC.jl/test/HEG/out", measurements, e)
 end
 
 function main()
     # MC options
-    NMC = 1 * 10^7
-    cyc = 10
+    NMC = 5 * 10^5
+    cyc = 5
     NEquil = 10^3
     # system parameters
     T_vals = [1.0]
     R_vals = [0.5]
-    dε = 4# 33 particles
+    dε = 4
     i = 0
     for th in T_vals
         for rs in R_vals
@@ -54,28 +57,5 @@ function main()
     end
 end
 
-"perform an MC run without taking measurements, thus profiling the MC process only"
-function profile_MC()
-    # MC options: perform only equlibration
-    NMC = 0
-    cyc = 1
-    NEquil = 10^7
-    # system parameters
-    theta = 1.0
-    rs = 0.5
-    dε = 4# 33 particles
-    S = get_sphere(Orbital((0,0,0),0),dε=dε)
-    N = length(S)
-    c = Configuration(S)
-    e = Ensemble(rs, get_beta_internal(theta,N), N) # get_beta_internal only works for 3D
-    updates = Update.([move_particle!], 0, 0)
-
-    measurements = Dict(
-        :Ekin => (Variance(), Ekin)
-    ,   :occs => (Group([Variance(UInt) for i = 1:100]), occupations)
-    )
-
-    sweep(NMC, cyc, NEquil, updates, measurements, e, c)
-end
 
 main()
