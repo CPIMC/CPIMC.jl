218cdf8f516bde4e175f99ad6952b15e00f1f67e
Paul Hamann <phamann@physik.uni-kiel.de>
2021-03-26 13:06:00 +0100

    move tests into the matching files

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ include("../src/Configuration.jl")
 include("../src/UEG/model.jl")
 
 
-tests = [ "test_configuration.jl" ]
+tests = [ "test_configuration.jl", "UEG/test_model.jl", "UEG/test_orbital.jl" ]
 
 for t in tests
     @testset "$t" begin
